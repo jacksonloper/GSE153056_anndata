3a783d9d8f2d39c7d77352420b94f59b446453b5
Jackson Loper <jl5116@columbia.edu>
2021-10-18 10:24:59 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='GSE153056_anndata',
+    author='',
+    version='0.0.1',
+    include_package_data=True,
+    description='',
+    packages=[
+        'GSE153056_anndata',
+    ],
+    package_data={
+        '': ['*.hdf5']
+    },
+)
